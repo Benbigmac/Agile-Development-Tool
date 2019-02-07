1475d6a4924477fce94b6360c219b9d86c4f32db
Benbigmac <benmaciorowski@yahoo.com>
2019-02-06 21:45:29 -0600

    Created WTForms forms for account login and creation

--- app.py
+++ app.py
@@ -8,3 +8,7 @@ def hello():
 @app.route("/Hello")
 def hello():
     return render_template('index.html')
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
---
+++ forms.py
@@ -0,0 +1,17 @@
+
+m flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+class LoginForm(FlaskForm):
+        username = StringField('Username', validators=[DataRequired()])
+        password = PasswordField('Password', validators=[DataRequired()])
+        submit = SubmitField('Sign In')
+
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('UserName')
+    password =StringField('Password')
+    name = StringField('Name')
+    submit = SubmitField("Create Account")
