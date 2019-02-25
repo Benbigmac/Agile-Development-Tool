24a96d3704bbca688f63815631c958db714b055b
Walden1995 <sew@jdwalden.com>
2019-02-25 12:39:41 -0600

    Added simple flask SQLAlchemy

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,16 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 from app import routes
+
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pyodbc://SEW-TABUTER/SQLEXPRESS/blueprism6.3'
+db = SQLAlchemy(app)
+
+SQLALCHEMY_TRACK_MODIFICATIONS = False
