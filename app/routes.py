99d7e21551783ac2a9c265e1200d2002b3e1a13b
Ben <benmaciorowski@yahoo.com>
2019-02-25 12:36:14 -0600

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,12 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 from app import app
 from app.forms import LoginForm, RegistrationForm
 
-
+loggedIn=False
 @app.route('/')
 def home():
-    return render_template("index.html")
+    form = RegistrationForm()
+    return render_template("index.html",title="FireScrum",form=form)
 
 @app.route('/signUp')
 def index():
@@ -13,7 +14,22 @@ def index():
     return render_template("signUp.html",form=form)
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
-    return render_template('signIn.html', title='Sign In', form=form)
+    if request.method == 'POST':
+        loggedIn=True
+        return redirect(url_for('projectList'))
+    else:
+        return render_template('signIn.html', title='Sign In', form=form)
+
+@app.route('/about')
+def about():
+    return render_template("aboutfirescrum.html")
+
+
+
+@app.route('/projects')
+def projectList():
+    projList=[{},{}]
+    return render_template("projects.html",projectList=projList)
