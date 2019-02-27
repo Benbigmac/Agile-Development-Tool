2c7e5506b25e3babe254519f845c74da025e7610
Walden1995 <sew@jdwalden.com>
2019-02-27 11:24:58 -0600

    imported sqlalchemy url setup

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,14 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+import urllib
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pyodbc://SEW-DESK/Firescrum'
+db = SQLAlchemy(app)
+
+
 
 from app import routes
