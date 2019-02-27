83eca50259736e4cec731a1e43d89c7638725665
Benbigmac <benmaciorowski@yahoo.com>
2019-02-26 19:48:44 -0600

    Major Overhaul of look and feel of project

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,18 @@
-from flask import render_template, flash, redirect, url_for, request
+from flask import render_template, flash, redirect, url_for, request,session
 from app import app
 from app.forms import LoginForm, RegistrationForm
+import json
 
 loggedIn=False
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    form = RegistrationForm()
-    return render_template("index.html",title="FireScrum",form=form)
+    form = LoginForm()
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        loggedIn=True
+        return redirect(url_for('projectList'))
+    else:
+        return render_template("index.html",title="FireScrum",form=form,username="Ben")
 
 @app.route('/signUp')
 def index():
@@ -18,18 +24,24 @@ def index():
 def login():
     form = LoginForm()
     if request.method == 'POST':
+        session['username'] = request.form['username']
         loggedIn=True
         return redirect(url_for('projectList'))
     else:
-        return render_template('signIn.html', title='Sign In', form=form)
+        return render_template('signIn.html', title='Sign In', form=form,username="Ben")
 
 @app.route('/about')
 def about():
     return render_template("aboutfirescrum.html")
 
-
-
 @app.route('/projects')
 def projectList():
-    projList=[{},{}]
-    return render_template("projects.html",projectList=projList)
+    username = request.cookies.get('username')
+    projList=[{"name":"FireScrum","description":"IT's WHAT YOU're USING!"},{"name":"D&D Web App","description":"We're working on stuff here"}]
+#    projList=json.dumps(projList)
+    print (projList)
+    return render_template("projects.html",projectList=projList, username="Ben")
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('error.html'), 404
