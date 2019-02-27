f7a2ac14c14bcdbca8cdd7d21f43dbd3084944b6
BuildTools <unconfigured@null.spigotmc.org>
2019-02-27 12:46:48 -0600

    added basic setup for database api

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import urllib
 
 app = Flask(__name__)
@@ -9,6 +10,8 @@ app.config.from_object(Config)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pyodbc://SEW-DESK/Firescrum'
 db = SQLAlchemy(app)
 
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+migrate = Migrate(app,db)
 
-from app import routes
+from app import routes #, models <- to be implmented
