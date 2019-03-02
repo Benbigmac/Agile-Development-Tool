e40074cbe1aceeb46a96a9d40581eb8439bcf297
Stephen Walden <swalde3@uic.edu>
2019-03-02 17:33:33 -0600

    Model needs to be looked at. Causes unknown crash. (#54)
    * imported sqlalchemy url setup
    
    * added basic setup for database api
    
    * Modified Git
    
    * added .gitignore rule for vscode 2
    
    * removing .vscode settings
    
    * Finished out Table Modeling, Implmented rest of table
    
    * Something about models import is broken
    
    * fixed issues with models.py

---
+++ app/models.py
@@ -0,0 +1,51 @@
+from datetime import datetime
+from app import db
+
+
+class Accounts(db.Model):
+    AccID = db.Column(db.Integer, primary_key=True, index=True, unique=True)
+    fname = db.Column(db.String(20), index=True)
+    lname = db.Column(db.String(20), index=True)
+    username = db.Column(db.String(20), index=True, unique=True)
+    password = db.Column(db.String(20))
+    email = db.Column(db.String(50), index=True, unique=True)
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Accounts {}>'.format(self.username)
+
+
+class Projects(db.Model):
+    name = db.Column(db.String(50))
+    ProjectID = db.Column(db.Integer, primary_key=True,
+                          index=True, unique=True)
+    description = db.Column(db.String(50))
+    start_date = db.Column(db.DateTime, index=True)
+
+    def __repr__(self):
+        return '<Projects {}>'.format(self.ProjectID)
+
+
+class Stories(db.Model):
+    ProjectID = db.Column(db.Integer, db.ForeignKey('Projects.id'))
+    description = db.Column(db.String(50))
+    StoryID = db.Column(db.Integer, primary_key=True, index=True, unique=True)
+
+    def __repr__(self):
+        return '<Stories {}>'.format(self.StoryID)
+
+
+class Tasks(db.Model):
+    TaskID = db.Column(db.Integer, primary_key=True, index=True, unique=True)
+    StoryID = db.Column(db.Integer, db.ForeignKey('Stories.StoryID'))
+    description = db.Column(db.String(50))
+    effort = db.Column(db.Integer)
+
+    def __repr__(self):
+        return '<Tasks {}>'.format(self.TaskID)
+
+
+class Userprojects(db.Model):
+    ident = db.Column(db.Integer, primary_key=True, index=True, unique=True)
+    ProjectID = db.Column(db.Integer, db.ForeignKey('Projects.ProjectID'))
+    AccID = db.Column(db.Integer, db.ForeignKey('Accounts.AccID'))
