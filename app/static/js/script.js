24f1dfc6d23355380a7e274407ce495025e17d97
Tian Zhou <tian243@gmail.com>
2019-04-04 13:18:08 -0500

    removed duplicate script from script.js

--- app/static/js/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-	var draggie = new Draggable(document.querySelectorAll('ul'), {
-	  draggable: 'li'
-	});
-
-	draggable.on('drag:start', () => console.log('drag:start'));
-	draggable.on('drag:move', () => console.log('drag:move'));
-	draggable.on('drag:stop', () => console.log('drag:stop'));
-});
