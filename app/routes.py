b23c963f341e05e4cdbdad9ce55ce3ddf57636bf
lhmzhou <lhmzhou@gmail.com>
2019-04-08 09:38:56 -0500

    Account settings (#80)
    * added page for account settings
    
    * more changes to account settings page
    kfjdlksfjsd

--- app/routes.py
+++ app/routes.py
@@ -4,8 +4,8 @@ from app.forms import LoginForm, RegistrationForm, ProjectForm
 import json, sys
 
 current_user=""
-#dataString='/Users/lzhou/Documents/group7/app/data/'
-dataString='C:/Users/benma/Desktop/cs442/code/group7/app/data/'
+dataString='/Users/lzhou/Documents/group7/app/data/'
+#dataString='C:/Users/benma/Desktop/cs442/code/group7/app/data/'
 #dataString='C:/Users/swald/group7/app/data/'
 @app.route('/', methods=['GET', 'POST'])
 def home():
